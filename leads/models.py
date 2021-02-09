e731840ca85cd0f298d941afa8e531b9f8e28fea
entirelymagic@gmail.com <@lexandr@123>
2021-02-09 09:36:04 +0200

    Added first Model Lead to models and made migration

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Lead(models.Model):
+    """A Lead database with first name, last name and age."""
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    age = models.IntegerField(default=0)
+
