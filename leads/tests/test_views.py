74fde2a61d7247ff937b55059f23349c8b58d1b4
entirelymagic@gmail.com <@lexandr@123>
2021-02-18 09:08:52 +0200

    confi

--- leads/tests/test_views.py
+++ leads/tests/test_views.py
@@ -1,10 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.shortcuts import reverse
 
 
 class LandingPageTest(TestCase):
 
-    def test_get(self):
-        response = self.client.get(reverse("landing-page"))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "landing.html")
+    # def decorator_get(self):
+    #     response = self.client.get(reverse("landing_page"))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, "landing.html")
+    #
+
+    def _decorator_get(self):
+        def wrapper(url, html_page):
+            response = self.client.get(reverse(url))
+            self.assertEqual(response.status_code, 200)
+            self.assertTemplateUsed(response, html_page)
+
+        return wrapper
+
+    @_decorator_get
+    def test_get(self, url="landing_page", html_page="landing.html"):
+        pass
