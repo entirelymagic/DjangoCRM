36e01fbe31a4b5a8fc2740c6f47fc1513372203f
entirelymagic@gmail.com <@lexandr@123>
2021-02-18 10:01:49 +0200

    Checked Authentication.

--- leads/tests/test_views.py
+++ leads/tests/test_views.py
@@ -4,20 +4,10 @@ from django.shortcuts import reverse
 
 class LandingPageTest(TestCase):
 
-    # def decorator_get(self):
-    #     response = self.client.get(reverse("landing_page"))
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, "landing.html")
-    #
+    def test_get(self):
+        response = self.client.get(reverse("landing_page"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "landing.html")
 
-    def _decorator_get(self):
-        def wrapper(url, html_page):
-            response = self.client.get(reverse(url))
-            self.assertEqual(response.status_code, 200)
-            self.assertTemplateUsed(response, html_page)
 
-        return wrapper
 
-    @_decorator_get
-    def test_get(self, url="landing_page", html_page="landing.html"):
-        pass
