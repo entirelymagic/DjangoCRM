9bbe9711acd63bea792c61d43992c474e44edbef
entirelymagic@gmail.com <@lexandr@123>
2021-02-23 09:55:04 +0200

    Added agent create and view forms.

--- agents/views.py
+++ agents/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import reverse
 from leads.models import Agent
+from .forms import AgentModelForm
 
 
 # Create your views here.
@@ -10,3 +11,11 @@ class AgentListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return Agent.objects.all()
+
+
+class AgentCreateView(LoginRequiredMixin, generic.CreateView):
+    template_name = 'agents/agent_create.html'
+    form_class = AgentModelForm
+
+    def get_success_url(self):
+        reverse("agents:agent-list")
